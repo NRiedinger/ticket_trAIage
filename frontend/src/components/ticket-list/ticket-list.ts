02c4cb5f4cbd6a4c1e18f189275f3b051e0907ea
Niklas <niklasriedinger@hotmail.com>
2026-01-31 17:08:41 +0100

    added PrimeNG to frontend

---
+++ frontend/src/components/ticket-list/ticket-list.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {ButtonModule} from 'primeng/button';
+
+@Component({
+  selector: 'app-ticket-list',
+  imports: [ButtonModule],
+  templateUrl: './ticket-list.html',
+  styleUrl: './ticket-list.css',
+})
+export class TicketList {
+
+}
